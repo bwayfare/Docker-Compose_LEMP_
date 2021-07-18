dd015c26568c1d770bd0a429b26a20fb84a6b628
bwayfare <bwayfare@student.21-school.ru>
2021-07-18 18:51:50 -0400

    Need syntax sugar

--- srcs/wordpress/conf/wp-config.php
+++ srcs/wordpress/conf/wp-config.php
@@ -19,16 +19,16 @@
 
 //  MySQL settings - You can get this info from your web host  //
 /*  The name of the database for WordPress */
-define( 'DB_NAME', 'wordpress' );
+define( 'DB_NAME', 'CONF_NAME' );
 
 /* MySQL database username */
-define( 'DB_USER', 'root' );
+define( 'DB_USER', 'CONF_USER' );
 
 /* MySQL database password */
-define( 'DB_PASSWORD', '' );
+define( 'DB_PASSWORD', 'CONF_PASS' );
 
 /* MySQL hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'CONF_HOST' );
 
 /* Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
--- srcs/wordpress/script/start.sh
+++ srcs/wordpress/script/start.sh
@@ -1,1 +1,5 @@
+sed -i -e "s|CONF_NAME|$DB_NAME|g" /var/www/my_site/wp-config.php
+sed -i -e "s|CONF_USER|$DB_USER|g" /var/www/my_site/wp-config.php
+sed -i -e "s|CONF_PASS|$DB_PASSWORD|g" /var/www/my_site/wp-config.php
+sed -i -e "s|CONF_HOST|$DB_HOST|g" /var/www/my_site/wp-config.php
 /usr/sbin/$(ls /usr/sbin/ |grep php | grep fpm) --nodaemonize
