5e98ac74086dc4a1f67859b6b98d64559640e859
bwayfare <bwayfare@student.21-school.ru>
2021-07-18 19:14:33 -0400

    Need syntax sugar, deleted tail in mariadb

--- srcs/mariaDB/script/start.sh
+++ srcs/mariaDB/script/start.sh
@@ -4,4 +4,4 @@ sed -i -e "s|CONF_USER|$DB_USER|g" wp_database.sql
 sed -i -e "s|CONF_PASS|$DB_PASSWORD|g" wp_database.sql
 sed -i -e "s|CONF_HOST|$DB_HOST|g" wp_database.sql
 mysql  < wp_database.sql
-tail -F /var/log/mysql/error.log
+/usr/bin/mysqld_safe
