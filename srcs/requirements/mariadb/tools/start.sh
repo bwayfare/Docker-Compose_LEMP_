7581f6bc6621a565e59d91ac30cdf0cf2ec83f61
bwayfare <bwayfare@student.21-school.ru>
2021-07-20 15:19:07 -0400

    Good, need test

--- srcs/requirements/mariadb/tools/start.sh
+++ srcs/requirements/mariadb/tools/start.sh
@@ -1,7 +1,15 @@
+mkdir -p /var/run/mysqld/
+mkfifo /var/run/mysqld/mysqld.sock
+chmod 777 /var/run/mysqld/mysqld.sock
+chown -R mysql /var/run/mysqld
+
 service mysql start
+
 sed -i -e "s|CONF_NAME|$DB_NAME|g" wp_database.sql
 sed -i -e "s|CONF_USER|$DB_USER|g" wp_database.sql
 sed -i -e "s|CONF_PASS|$DB_PASSWORD|g" wp_database.sql
 sed -i -e "s|CONF_HOST|$DB_HOST|g" wp_database.sql
+
 mysql  < wp_database.sql
+
 /usr/bin/mysqld_safe
--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -1,5 +1,10 @@
+mkdir -p /run/php/
+sed -i 's/;daemonize = yes/daemonize = no/g' /etc/php/7.3/fpm/pool.d/www.conf
+echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
+
 sed -i -e "s|CONF_NAME|$DB_NAME|g" /var/www/my_site/wp-config.php
 sed -i -e "s|CONF_USER|$DB_USER|g" /var/www/my_site/wp-config.php
 sed -i -e "s|CONF_PASS|$DB_PASSWORD|g" /var/www/my_site/wp-config.php
 sed -i -e "s|CONF_HOST|$DB_HOST|g" /var/www/my_site/wp-config.php
+
 /usr/sbin/$(ls /usr/sbin/ |grep php | grep fpm) --nodaemonize
