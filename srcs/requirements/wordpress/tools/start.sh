2cc5769b73813118845ef277ed76b5ab81f44e9b
bwayfare <bwayfare@student.21-school.ru>
2021-07-21 22:09:51 +0300

    Need add if-else and users

--- srcs/requirements/wordpress/tools/start.sh
+++ srcs/requirements/wordpress/tools/start.sh
@@ -5,6 +5,6 @@ echo "listen = 9000" >> /etc/php/7.3/fpm/pool.d/www.conf
 sed -i -e "s|CONF_NAME|$DB_NAME|g" /var/www/my_site/wp-config.php
 sed -i -e "s|CONF_USER|$DB_USER|g" /var/www/my_site/wp-config.php
 sed -i -e "s|CONF_PASS|$DB_PASSWORD|g" /var/www/my_site/wp-config.php
-sed -i -e "s|CONF_HOST|$DB_HOST|g" /var/www/my_site/wp-config.php
+sed -i -e "s|CONF_HOST|$DB_HOST:$DB_PORT|g" /var/www/my_site/wp-config.php
 
 /usr/sbin/$(ls /usr/sbin/ |grep php | grep fpm) --nodaemonize
